91bc18efa04e8c9078d5bfcaeb88d2a35bf34c8c
Michael Jackson <mj94@mj94.net>
2017-08-14 18:18:52 -0500

    Send recorded audio file

--- PitchPerfect/RecordSoundsViewController.swift
+++ PitchPerfect/RecordSoundsViewController.swift
@@ -57,7 +57,11 @@ class RecordSoundsViewController: UIViewController, AVAudioRecorderDelegate {
     }
 
     func audioRecorderDidFinishRecording(_ recorder: AVAudioRecorder, successfully flag: Bool) {
-        print("Finished recording.")
+        if flag {
+            performSegue(withIdentifier: "stopRecording", sender: audioRecorder.url)
+        } else {
+            print("The recording was not successful.")
+        }
     }
     
 }
